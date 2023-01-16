b2ed1a6d1603d695d4688103baf2c24c2acf325f
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2023-01-16 15:37:20 -0300

    trying to fix the seconds error

--- assets/js/time.js
+++ assets/js/time.js
@@ -30,5 +30,5 @@ function displayClock() {
 	document.getElementById('month').innerText = mm;
 	document.getElementById('day').innerText = dd;
 
-	setTimeout(displayClock, 1000);
+	setInterval(displayClock, 1000);
 }
